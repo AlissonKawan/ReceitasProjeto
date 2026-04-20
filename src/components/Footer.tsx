75217bf34155b5a159c7f94bd79b643fff61bbc0
marcos-thebest <contato.marcosvinicius2004@gmail.com>
2026-04-20 16:20:22 -0300

    Atualizando o Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,12 @@
 function Footer() {
     return(
         <>
-            <footer className="bg-red-900">
+            <footer className="bg-orange-800">
                 <section>
                     <article>
                         <div>
-                            <h4 className="text-white text-1xl font-extrabold tracking-wide drop-shadow-lg text-center m-4 p-5">Copyright © 2026, Receitas Web. All Rights Reserved.</h4>
-                            <h5 className="text-white text-1xl font-extrabold tracking-wide drop-shadow-lg text-center pb-5">
+                            <h4 className="text-white text-1xl tracking-wide drop-shadow-lg text-center m-4 p-5">Copyright © 2026, Receitas Web. All Rights Reserved.</h4>
+                            <h5 className="text-white text-1xl tracking-wide drop-shadow-lg text-center pb-5">
                                 Created by
                                     Marcos Vinicius, Milena Pamio, Eduardo Boni and Alisson Kawan
                             </h5>
