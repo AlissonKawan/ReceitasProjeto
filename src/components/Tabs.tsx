e41744082519dc2c01f7539bbcafc92824b3d951
bonieduardo75 <bonieduardo75@gmail.com>
2026-04-21 18:50:52 -0300

    teste5

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,7 +1,7 @@
 function Tabs() {
     return(
         <>
-        <h1>Teste2, teste3, teste4</h1>
+        <h1>Teste2, teste3, teste4, teste5</h1>
         <h2>Alisson testando</h2>
         </>
     )
